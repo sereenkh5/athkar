e94f0c57af087a9f37346028ab6447efb76a32e8
sereenkh5 <152020040+sereenkh5@users.noreply.github.com>
2024-02-17 12:55:24 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,94 @@
+
+
+
+
+
+
+
+
+
+const clic=document.querySelector(".clic");
+let pointer=0 ;
+console.log(pointer)
+clic.onclick=function(e){
+    console.log(pointer)
+    ++pointer ;
+    console.log(pointer)
+    document.querySelector(".pointer").innerHTML=pointer;
+
+}
+
+const startagin=document.querySelector(".staragin");
+startagin.onclick=function(e){
+    console.log(pointer)
+    pointer=0 ;
+    console.log(pointer)
+    document.querySelector(".pointer").innerHTML=pointer;
+
+}
+
+
+
+
+const clic1=document.querySelector(".clic1");
+let pointer1=0 ;
+console.log(pointer1)
+clic1.onclick=function(e){
+    console.log(pointer1)
+    ++pointer1 ;
+    console.log(pointer1)
+    document.querySelector(".pointer1").innerHTML=pointer1;
+
+}
+
+const startagin1=document.querySelector(".staragin1");
+startagin1.onclick=function(e){
+    console.log(pointer1)
+    pointer1=0 ;
+    console.log(pointer1)
+    document.querySelector(".pointer1").innerHTML=pointer1;
+
+}
+
+
+const clic2=document.querySelector(".clic2");
+let pointer2=0 ;
+console.log(pointer2)
+clic2.onclick=function(e){
+    console.log(pointer2)
+    ++pointer2 ;
+    console.log(pointer2)
+    document.querySelector(".pointer2").innerHTML=pointer2;
+
+}
+
+const startagin2=document.querySelector(".staragin2");
+startagin2.onclick=function(e){
+    console.log(pointer2)
+    pointer2=0 ;
+    console.log(pointer2)
+    document.querySelector(".pointer2").innerHTML=pointer2;
+
+}
+
+
+
+const clic3=document.querySelector(".clic3");
+let pointer3=0 ;
+console.log(pointer3)
+clic3.onclick=function(e){
+    console.log(pointer3)
+    ++pointer3 ;
+    console.log(pointer3)
+    document.querySelector(".pointer3").innerHTML=pointer3;
+
+}
+
+const startagin3=document.querySelector(".staragin3");
+startagin3.onclick=function(e){
+    console.log(pointer3)
+    pointer3=0 ;
+    console.log(pointer3)
+    document.querySelector(".pointer3").innerHTML=pointer3;
+
+}
